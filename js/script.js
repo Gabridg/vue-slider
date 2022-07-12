7b956a8ce0db2896511102820f6aa200057f63b8
Gabridg <gabri99.del@gmail.com>
2022-07-12 16:27:04 +0200

    added thumbnails and connected w/ Vue

--- js/script.js
+++ js/script.js
@@ -19,50 +19,41 @@ Consigli del giorno:
 Buon lavoro e buon divertimento!
 */
 
+const root = new Vue({
+    el: '#root',
+    data: {
+        pictures: [
+            {
+                url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+                title: 'Svezia',
+                description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
+            },
 
-const source = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg", "img/06.jpg", "img/07.jpg", "img/08.jpg", "img/09.jpg", "img/10.jpg"];
+            {
+                url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+                title: 'Perù',
+                description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
+            },
 
-const carousel = document.getElementById('carousel');
-const nextButton = document.getElementById('next');
-const backButton = document.getElementById('back');
+            {
+                url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+                title: 'Chile',
+                description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
+            },
+            {
+                url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+                title: 'Argentina',
+                description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
+            },
+            {
+                url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+                title: 'Colombia',
+                description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
+            },
+        ],
+        currentActiveIndex: 0,
+    },
+    methods: {
 
-let imageElement = '';
-
-for (let i = 0; i < source.length; i++) {
-    imageElement += `<img src=${source[i]} alt="landscape ${i}" />`;
-}
-
-carousel.innerHTML = imageElement;
-
-const images = document.querySelectorAll('#carousel img');
-console.log(images);
-
-
-let currentActiveIndex = 0;
-
-images[currentActiveIndex].classList.add('active');
-
-nextButton.addEventListener('click', function () {
-    images[currentActiveIndex].classList.remove('active');
-
-    currentActiveIndex++;
-
-    if (currentActiveIndex === images.length) {
-        currentActiveIndex = 0;
     }
-
-    images[currentActiveIndex].classList.add('active');
-})
-
-
-backButton.addEventListener('click', function () {
-    images[currentActiveIndex].classList.remove('active');
-
-    currentActiveIndex--;
-
-    if (currentActiveIndex < 0) {
-        currentActiveIndex = images.length - 1;
-    }
-
-    images[currentActiveIndex].classList.add('active');
 })
